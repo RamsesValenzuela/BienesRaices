49574d781145505c669843111c4d7b4d29bfab89
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-09 03:24:31 -0700

    Create Category.js

---
+++ models/Category.js
@@ -0,0 +1,11 @@
+import {DataTypes} from 'sequelize'
+import db from '../config/db'
+
+const Category = db.define('categorys', {
+    name:{
+     type: DataTypes.STRING(30),
+     allowNull:false
+    }
+})
+
+export default Category

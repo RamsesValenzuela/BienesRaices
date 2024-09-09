bd3b9a396749426bb82dcd08947ff0f13205cbd5
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-09 03:24:33 -0700

    Create Estate.js

---
+++ models/Estate.js
@@ -0,0 +1,56 @@
+import {DataTypes} from 'sequelize'
+import db from '../config/db'
+
+
+const Estate = db.define('estate',{
+    id:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    description:{
+        type: DataTypes.TEXT,
+        allowNull:false,
+    },
+    roomQty:{
+        type: DataTypes.DECIMAL,
+        allowNull:false
+    },
+    garage:{
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    wc:{
+        type: DataTypes.DECIMAL,
+        allowNull:false
+    },
+    street:{
+        type:DataTypes.STRING(60),
+        allowNull:false
+    },
+    lat:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    lng:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    imagen:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    published:{
+        type: DataTypes.BOOLEAN,
+        allowNull:false,
+        defaultValue: false
+    }
+
+})
+
+export default Estate

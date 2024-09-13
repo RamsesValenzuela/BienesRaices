40e689ed9236992c3a999dcce25d6187909cd2f7
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-13 14:41:20 -0700

    Update estateController.js
    data query in the categories and prices database added to the code

--- controllers/estateController.js
+++ controllers/estateController.js
@@ -1,3 +1,9 @@
+import db from '../config/db.js'
+import Price from '../models/Price.js'
+import Category from '../models/Category.js'
+import { promises } from 'nodemailer/lib/xoauth2'
+
+
 const admin = (req, res) =>{
     res.render('estate/admin',{
         pagina: 'Mis Propiedades',
@@ -5,10 +11,20 @@ const admin = (req, res) =>{
     })
 }
 
-const crear = (req, res) =>{
+const crear =  async (req, res) =>{
+
+    //consultar modelo de Precio y Categorias
+    const [categorys, prices] = await promises.all([
+        Category.findAll(),
+        Price.findAll(),
+    ])
+
     res.render('estate/create', {
         pagina: 'Crear Propiedad',
-        barra: true
+        barra: true,
+        categorys,
+        prices
+
     })
 }
 

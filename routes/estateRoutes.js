86040cfd580f7ab686cf90230476f131686b63d5
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-14 22:34:45 -0700

    Update estateRoutes.js
    import the method from the controller

--- routes/estateRoutes.js
+++ routes/estateRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {admin, crear} from '../controllers/estateController.js'
+import {admin, crear, saveEstate} from '../controllers/estateController.js'
 
 const router = express.Router()
 
 router.get('/mis_propiedades', admin)
 router.get('/propiedades/crear', crear)
+router.post('/propiedades/crear', saveEstate)
 
 
 export default router

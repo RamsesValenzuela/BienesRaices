0dab3ed258fb734c3a992cf5c5688d4cee6de4ef
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-16 23:55:49 -0700

    Update estateRoutes.js
    express validator configuration for error in the path to post a new publication

--- routes/estateRoutes.js
+++ routes/estateRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import { body } from 'express-validator'
 import {admin, crear, saveEstate} from '../controllers/estateController.js'
 
 const router = express.Router()
 
 router.get('/mis_propiedades', admin)
 router.get('/propiedades/crear', crear)
-router.post('/propiedades/crear', saveEstate)
+router.post('/propiedades/crear', 
+    body('titulo').notEmpty().withMessage('El Titulo Del Anuncio Es Obligatorio'),
+    saveEstate)
 
 
 export default router

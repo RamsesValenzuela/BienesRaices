d3f395ba4fc966a82982a4f6560f50fe77348020
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-18 14:22:22 -0700

    Create User.js

---
+++ seed/User.js
@@ -0,0 +1,10 @@
+import bcrypt from 'bcrypt' 
+
+const usuarios = [{
+    nombre: 'juan',
+    email : 'juan@gmail.com',
+    confirmado : 1,
+    password: bcrypt.hashSync('password', 10)
+}]
+
+export default usuarios

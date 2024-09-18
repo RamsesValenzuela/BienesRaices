d09385364566296a3c0e9d629e46608d5a34639e
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-18 16:56:10 -0700

    Create ProtectPath.js

---
+++ middleware/ProtectPath.js
@@ -0,0 +1,11 @@
+const protectPath = async (req, res, next) =>{
+    console.log('Desde el middleware')
+
+    //verificar si hay un token
+
+
+    //comprobar el token
+    next()
+}
+
+export default protectPath

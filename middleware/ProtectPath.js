00970ad86c59cd290897f1e27ace5f04c6e8093e
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-19 19:40:14 -0700

    Update ProtectPath.js
    Middleware for protection of routes was already finished

--- middleware/ProtectPath.js
+++ middleware/ProtectPath.js
@@ -1,9 +1,34 @@
+import jwt from 'jsonwebtoken'
+import  Usuario   from '../models/Usuario.js'
+
 const protectPath = async (req, res, next) =>{
     console.log('Desde el middleware')
 
     //verificar si hay un token
+    const {_token} = req.cookies
+
+    if(!_token){
+        return res.redirect('/auth/login')
+    }
 
 
+    try {
+        
+        const decoded = jwt.verify(_token, process.env.SECRETWORD)
+        const user = await Usuario.scope('deletePassword').findByPk(decoded.id)
+        
+       //Almacenar el usuario al Req
+       if(user){
+            req.usuario = user
+       }else{
+            return res.redirect('/auth/login')
+       }
+       
+       return next()
+
+    } catch (error) {
+        return res.clearCookie('_token').redirect('/auth/login')
+    }
     //comprobar el token
     next()
 }
